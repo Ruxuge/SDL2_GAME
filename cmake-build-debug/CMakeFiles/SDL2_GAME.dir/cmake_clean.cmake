49e78566d7678723d9294a8e874790bbfec3beb4
Ruxuge <filipwerra.12@gmail.com>
2021-06-10 09:19:55 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/SDL2_GAME.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL2_GAME.dir/main.cpp.o"
+  "SDL2_GAME"
+  "SDL2_GAME.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL2_GAME.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
